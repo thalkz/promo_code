9798bae1d645e6232467d81b23ff9f45cd8fba5d
Roland Lamidieu <roland.lamidieu@gmail.com>
2023-12-28 15:33:10 +0100

    feat: Implement /add endpoint

--- endpoints/add.go
+++ endpoints/add.go
@@ -1,9 +1,25 @@
 package endpoints
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/thalkz/promo_code/database"
+	"github.com/thalkz/promo_code/promocode"
 )
 
 func HandleAdd(c *gin.Context) {
-	// TODO Implement me
+	var request promocode.Promocode
+	err := c.Bind(&request)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"promocode_name": request.Name,
+			"status":         "bad request",
+			"reason":         err.Error(),
+		})
+		return
+	}
+
+	database.Instance[request.Name] = &request
 }
